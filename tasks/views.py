e181a1238f3d286db7c924d4a031be8554edb671
ingen1us <yeinernaranjo@gmail.com>
2022-10-31 17:38:59 -0500

    Se añade logout y nueva pestaña de Tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,25 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login, logout
+from django.db import IntegrityError
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+
 def signup(request):
 
     if request.method == 'GET':
-        return render(request, 'signup.html', {
-            'form': UserCreationForm
-        })
+        return render(request, 'signup.html', {'form': UserCreationForm})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            # register user
             try:
-                User.objects.create_user(username = request.POST['username'], password = request.POST['password1'])
-                User.save()
-                return HttpResponse('User created succesfully')
-            except:
-                return HttpResponse('Username already exists')
-        return HttpResponse('Password do not match')
+                # register user
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Username already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
+
+
+def tasks(request):
+    return render(request, 'tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
